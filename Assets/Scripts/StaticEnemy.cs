55f776ab16b54682d0693688c6ddbf4ccbab0a47
danielarago <danielarago@unisabana.edu.co>
2022-04-19 14:37:11 -0500

    Avances enemigo estático

--- Assets/Scripts/StaticEnemy.cs
+++ Assets/Scripts/StaticEnemy.cs
@@ -10,14 +10,15 @@ public class StaticEnemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
     void Update()
     {
         RaycastHit2D ray = Physics2D.Raycast(transform.position, Vector2.left, 3.2f, LayerMask.GetMask("Player"));
-        if (ray == true)
+        
+        if (ray.collider != null)
         {
             Instantiate(bullet, transform.position, transform.rotation);
         }
--- Assets/Scripts/TurretBullet.cs
+++ Assets/Scripts/TurretBullet.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class TurretBullet : MonoBehaviour
 {
+    int bulletSpeed = -4;
+    Rigidbody2D myBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        myBody.velocity = new Vector2(bulletSpeed, 0);
     }
 }
