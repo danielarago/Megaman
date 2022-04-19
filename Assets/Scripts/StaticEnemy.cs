01dedf62e474e17d5a96d9f32a25168cb8c015af
danielarago <danielarago@unisabana.edu.co>
2022-04-19 11:48:11 -0500

    Cambios enemigo estático

--- Assets/Scripts/StaticEnemy.cs
+++ Assets/Scripts/StaticEnemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class StaticEnemy : MonoBehaviour
 {
+    [SerializeField] int life;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class StaticEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, Vector2.down, 1.3f, LayerMask.GetMask("Ground"));
         
     }
 }
