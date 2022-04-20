a8315e21b6a17ab4e11ab2a0b83decf6cd08293e
danielarago <danielarago@unisabana.edu.co>
2022-04-19 22:16:48 -0500

    Correcciones

--- Assets/Scripts/StaticEnemy.cs
+++ Assets/Scripts/StaticEnemy.cs
@@ -8,27 +8,29 @@ public class StaticEnemy : MonoBehaviour
     [SerializeField] GameObject bullet;
     [SerializeField] float fireRate;
     float allowFire;
+    Animator myAnim;
 
     // Start is called before the first frame update
     void Start()
     {
-     
+        myAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D ray = Physics2D.Raycast(transform.position, Vector2.left, 3.2f, LayerMask.GetMask("Player"));
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, Vector2.left, 4f, LayerMask.GetMask("Player"));
         
         if ((ray.collider != null) && (Time.time > allowFire))
         {
             Instantiate(bullet, transform.position, transform.rotation);
             allowFire = Time.time + fireRate;
+            Debug.Log("Jugador en mira turret");
         }
 
         if (life == 0)
         {
-            Destroy(gameObject);
+            myAnim.SetBool("hasDied", true);
         }
     }
 
@@ -38,5 +40,10 @@ public class StaticEnemy : MonoBehaviour
         {
             life--;
         }
+    }
+
+    public void Death()
+    {
+        Destroy(gameObject);
     }
 }
