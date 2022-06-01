a4869b6d473b7c4f1d2bf021b46c7c83a2c51d38
danielarago <danielarago@unisabana.edu.co>
2022-05-31 23:25:42 -0500

    Cambios entre escenas

--- Assets/UI Toolkit/UIControllerPF.cs
+++ Assets/UI Toolkit/UIControllerPF.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class UIControllerPF : MonoBehaviour
 {
+    Button btn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        btn = root.Q<Button>("btn");
+
+        btn.clicked += ReloadGame;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void ReloadGame()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
